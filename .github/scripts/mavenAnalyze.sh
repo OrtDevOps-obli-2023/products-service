6c307f5ce61eb5ff93d7165783e64e24a25c13fa
Sebastian Piazza <sebastian@adermarkarian23gmail.onmicrosoft.com>
2023-07-18 19:14:43 -0300

    change for sonarcloud

--- .github/scripts/mavenAnalyze.sh
+++ .github/scripts/mavenAnalyze.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./mvnw -B verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=$PROJECT_KEY -Dsonar.organization=$ORG_NAME -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
+./mvn -B verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=$PROJECT_KEY -Dsonar.organization=$ORG_NAME -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN
